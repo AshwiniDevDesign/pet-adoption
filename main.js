02f79027df8a47450bc17e3a3349e326ab571bcb
Ashwini <ashwinirbhat8@gmail.com>
2024-03-29 17:49:31 +0530

    changed the age and photo of cats

--- main.js
+++ main.js
@@ -19,8 +19,26 @@ async function petsArea() {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
     clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-card-description").textContent = pet.description;
+    clone.querySelector(".pet-card-age").textContent = createAgeText(
+      pet.birthYear
+    );
+    clone.querySelector(".pet-card-image img").src = pet.photo;
+    clone.querySelector(
+      ".pet-card-image img"
+    ).alt = `A ${pet.species}  named ${pet.name}`;
     wrapper.appendChild(clone);
   });
   document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 petsArea();
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+
+  if (age == 1) return "1 year old";
+  if (age == 0) return "less than a year old";
+
+  return `${age}years old`;
+}
